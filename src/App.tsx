6e57a0e4620b6def76ee883342e524c0a933f060
Yann Davin <yanndvn@gmail.com>
2021-06-04 14:09:01 +0200

    [FIX] Global app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { FC } from "react";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { TopAppBar } from "./components/TopAppBar";
 import { Themes, themes } from "./utils/theme";
 import { Route, Router, Switch } from "react-router-dom";
@@ -8,12 +8,17 @@ import history from "./utils/history";
 import { HomePage } from "./pages/HomePage";
 import { MovieDetailPage } from "./pages/MovieDetailPage";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const App: FC = () => {
   // TODO: Default value to user's system preference
   const [pickedTheme, setTheme] = useState<Themes>(Themes.light);
   return (
     <ThemeProvider theme={themes[pickedTheme]}>
-      <>
+      <Container>
         <TopAppBar theme={pickedTheme} setTheme={setTheme} />
         <Router history={history}>
           <Switch>
@@ -31,7 +36,7 @@ const App: FC = () => {
             />
           </Switch>
         </Router>
-      </>
+      </Container>
     </ThemeProvider>
   );
 };
